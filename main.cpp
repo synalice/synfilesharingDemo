f4c6df24a42f9995356b4ddda05cd3b8d1ea7e87
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-04-12 14:57:40 +0300

    medium: add synfilesharing dependency

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <synfilesharing/synfilesharing.h>
 
 int main(int argc, char *argv[])
 {
+    synfs::helloWorld();
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
