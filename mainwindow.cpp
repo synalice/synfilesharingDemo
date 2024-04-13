e60ea14464a1d04a928d82011f2cccdcbb8def8a
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-04-13 11:58:28 +0300

    medium: add file selection from system file explorer

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,39 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <iostream>
+
+ #include <QFileDialog>
+ #include <QListWidget>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Добавление файлов
+    connect(
+        ui->selectFilesButton, &QPushButton::clicked,
+        this, &MainWindow::selectFiles
+    );
+
+    // Удаление файлов
+    connect(
+        ui->selectedFiles, &QListWidget::itemDoubleClicked,
+        this, &MainWindow::deleteClickedItem
+    );
+
+
+}
+
+void MainWindow::selectFiles() {
+    QList<QString> fileNames = QFileDialog::getOpenFileNames();
+    ui->selectedFiles->addItems(fileNames);
+}
+
+void MainWindow::deleteClickedItem(QListWidgetItem *item) {
+    ui->selectedFiles->takeItem(ui->selectedFiles->row(item));
 }
 
 MainWindow::~MainWindow()
