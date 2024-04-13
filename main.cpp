213d26fb006ae41650c2ceadfbeb093fc5f3637c
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-04-13 11:58:40 +0300

    medium: add server code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+
+#include <iostream>
+
 #include <synfilesharing/synfilesharing.h>
+
 
 int main(int argc, char *argv[])
 {
-    synfs::helloWorld();
+    std::vector<std::string> allowedFileExtensions = {".txt", ".md",};
+    auto receivedFiles = std::make_shared<std::vector<std::string>>();
+
+    std::unique_ptr<synfs::IServer> server = synfs::makeServer()
+        .setAllowedFileExtensions(allowedFileExtensions)
+        .setExecFlag(synfs::constants::DEFAULT_EXEC_FLAG)
+        .saveResultsTo(receivedFiles)
+        .build();
+
+    server->run(argc, argv);
 
     QApplication a(argc, argv);
     MainWindow w;
